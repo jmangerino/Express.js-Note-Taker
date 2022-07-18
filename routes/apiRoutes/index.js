87b9856e0c1edf80e0d87a9b63a353c92d21bb44
Josh Man <joshuamangerino@gmail.com>
2022-07-18 17:55:35 -0600

    finished routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,3 +3,33 @@ const db = require('../../db/db.json');
 const uniqid = require('uniqid');
 const router = require('express').Router();
 
+
+router.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json')
+    .then((data) => {
+        let notes = JSON.parse(data)
+        res.send(data)
+    })
+})
+
+
+router.post('./api/notes', (req, res) => {
+    let newNote = {
+        id: uniqid(),
+        title: req.body.title,
+        text: req.body.text
+    }
+
+    fs.readFile('./db/db.json', (err, data) => {
+        let newData = JSON.parse(data);
+
+        newData.push(newNote);
+        
+        fs.writeFile('./db/db.json', JSON.stringify(newData), (err) => {
+            if (err) throw err;
+            res.send('note added');
+        })
+    })
+})
+
+module.exports = router;

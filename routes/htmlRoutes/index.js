7df9d5eaadec4b9a513fd79f1cb0210b9c54a974
Josh Man <joshuamangerino@gmail.com>
2022-07-18 18:07:30 -0600

    fixed some mistakes

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const router = express.Router();
+const router = require('express').Router();
 
 router.get('./notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/notes.html'));
